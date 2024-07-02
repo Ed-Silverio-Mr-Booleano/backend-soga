176c717b6ed4ea9aee9bedd19ffcfe6ad42ebcec
Edmilson Silverio <edmilsondiabinho0@gmail.com>
2024-07-02 15:42:53 +0100

    refactor: add some fields in User migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,13 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('gender')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->date('phone_number')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('interesses')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -25,6 +32,15 @@ return new class extends Migration
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
