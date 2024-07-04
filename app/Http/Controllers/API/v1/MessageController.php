510b434020f0baa38fb68732685f03a70939812f
Ed-Silverio-Mr-Booleano <edmilsondiabinho0@gmail.com>
2024-07-04 03:48:27 +0000

    feat: create message controller - not tested

--- app/Http/Controllers/API/v1/MessageController.php
+++ app/Http/Controllers/API/v1/MessageController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class MessageController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of all messages.
      */
     public function index()
     {
-        //
+        $messages = Message::with(['sender', 'receiver'])->get();
+
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'data' => $messages
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -20,7 +31,35 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'receiverID' => 'required|exists:users,id',
+            'message' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
+        try {
+            $message = Message::create([
+                'senderID' => Auth::id(),
+                'receiverID' => $request->input('receiverID'),
+                'message' => $request->input('message'),
+                'sentDate' => now(),
+            ]);
+
+            return response()->json([
+                'status' => Response::HTTP_OK,
+                'message' => 'Message sent successfully',
+                'data' => $message
+            ], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Failed to send message',
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -28,15 +67,63 @@ class MessageController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $message = Message::with(['sender', 'receiver'])->find($id);
+
+        if (!$message) {
+            return response()->json([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => 'Message not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'data' => $message
+        ], Response::HTTP_OK);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Display all messages between two users.
      */
-    public function update(Request $request, string $id)
+    public function messagesBetweenUsers(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'userID1' => 'required|exists:users,id',
+            'userID2' => 'required|exists:users,id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
+        $messages = Message::where(function ($query) use ($request) {
+            $query->where('senderID', $request->input('userID1'))
+                ->where('receiverID', $request->input('userID2'));
+        })->orWhere(function ($query) use ($request) {
+            $query->where('senderID', $request->input('userID2'))
+                ->where('receiverID', $request->input('userID1'));
+        })->with(['sender', 'receiver'])->get();
+
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'data' => $messages
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Display all messages sent or received by a user.
+     */
+    public function messagesByUser(Request $request, string $id)
+    {
+        $messages = Message::where('senderID', $id)
+            ->orWhere('receiverID', $id)
+            ->with(['sender', 'receiver'])
+            ->get();
+
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'data' => $messages
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -44,6 +131,28 @@ class MessageController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $message = Message::findOrFail($id);
+
+            if ($message->senderID != Auth::id()) {
+                return response()->json([
+                    'status' => Response::HTTP_FORBIDDEN,
+                    'message' => 'You are not authorized to delete this message'
+                ], Response::HTTP_FORBIDDEN);
+            }
+
+            $message->delete();
+
+            return response()->json([
+                'status' => Response::HTTP_OK,
+                'message' => 'Message deleted successfully'
+            ], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Failed to delete message',
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
